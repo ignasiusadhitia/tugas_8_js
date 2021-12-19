93b50bc3495cc957d1b3e0c0ba99c3a0464687b8
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-20 04:48:33 +0700

    Pengumpulan Tugas 8 JS - NIOMIC

---
+++ tugas8.js
@@ -0,0 +1,21 @@
+let biodata = {
+  nama_lengkap: "Ignasius Yuda Adhitia",
+  tempat_tanggal_lahir: "Kuningan, 30 Juli 1987",
+  umur: 34,
+  pekerjaan: "Tutor Piano",
+};
+
+console.log(biodata);
+console.log("\n");
+
+// perubahan pada property pekerjaan
+
+biodata.pekerjaan = "Front End Web Developer";
+
+console.log(biodata);
+console.log("\n");
+
+// penambahan property hobi
+
+biodata.hobi = ["bermain piano", "ngoding", "menonton film"];
+console.log(biodata);
